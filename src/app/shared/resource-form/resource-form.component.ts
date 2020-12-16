57afeed66fb09cbab7a267c790be03dbe864d04c
Sidharth S <sidharths@qburst.com>
2020-12-16 18:30:04 +0530

    Add resource form validation

--- src/app/shared/resource-form/resource-form.component.ts
+++ src/app/shared/resource-form/resource-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { FormServiceService } from '../services/form-service.service';
 
@@ -12,6 +13,7 @@ export class ResourceFormComponent implements OnInit {
   buttonText: string;
   checkboxFlag: boolean;
   showRate = false;
+  resourceForm: FormGroup
 
   constructor(private router: Router, private formService: FormServiceService) { }
 
@@ -22,11 +24,23 @@ export class ResourceFormComponent implements OnInit {
     else {
       this.buttonText = 'Add Resource';
     }
+
+        // Form initialization
+        this.resourceForm = new FormGroup({
+          'resourceName': new FormControl(null, [Validators.required, Validators.pattern('^[a-zA-Z ]+$'), Validators.minLength(2)]),
+          'resourceEmail': new FormControl(null, [Validators.required, Validators.email]),
+          'checkboxFlag': new FormControl(null),
+          'billableAmount': new FormControl(0, [Validators.required, Validators.pattern('^[0-9]+$')])
+        })
   }
 
   cancelResource() {
     this.formService.isFormStatus.next(0);
     this.router.navigate(['/resource']);
+  }
+
+  onSubmit(){
+    console.log(this.resourceForm);
   }
 
 }
